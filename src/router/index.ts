9db76978bee410a685151904d8f2230487e5a612
Joice <anunes.joice@gmail.com>
2024-11-02 12:28:03 -0300

    config auto router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: h('p', { style: 'color: red;' }, 'Page not found'),
-    },
-  ],
+  routes,
 })
 
 export default router
