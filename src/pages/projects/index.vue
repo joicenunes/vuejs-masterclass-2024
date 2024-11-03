14087b457b1de32d5566d2e798e2de6408238b5f
Joice <anunes.joice@gmail.com>
2024-11-03 14:13:08 -0300

    feat: adding data table to projects page

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,19 +1,17 @@
 <template>
-  <div>
-    <h1>Projects View</h1>
-    <RouterLink to="/">Home</RouterLink>
-    <ul>
-      <li v-for="project in projects" :key="project.id">
-        {{ project.name }}
-      </li>
-    </ul>
-  </div>
+  <DataTable v-if="projects" :columns="columns" :data="projects" />
 </template>
 
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
 import type { Tables } from 'database/types'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
+import type { ColumnDef } from '@tanstack/vue-table'
+import {
+  mountTableCell,
+  mountTableHeader,
+} from '@/lib/helpers/dataTableFunctions'
 
 const projects = ref<Tables<'projects'>[] | null>(null)
 
@@ -23,6 +21,30 @@ const projects = ref<Tables<'projects'>[] | null>(null)
   if (error) console.log(error)
   projects.value = data
 })()
+
+const columns: ColumnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => mountTableHeader('Name'),
+    cell: ({ row }) => {
+      return mountTableCell(row.getValue('name'))
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => mountTableHeader('Status'),
+    cell: ({ row }) => {
+      return mountTableCell(row.getValue('status'))
+    },
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => mountTableHeader('Collaborators'),
+    cell: ({ row }) => {
+      return mountTableCell(JSON.stringify(row.getValue('collaborators')))
+    },
+  },
+]
 </script>
 
 <style lang=""></style>
